62e52f5f18ade498e409c328cbad56ea3236fa00
radha0202 <81341387+radha0202@users.noreply.github.com>
2025-10-29 19:20:52 +0530

    update layout.js

--- src/app/products/layout.js
+++ src/app/products/layout.js
@@ -1,4 +1,3 @@
-import CategoryNav from "@/components/CategoryNav";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
@@ -6,7 +5,6 @@ export default function ProductsLayout({ children }) {
   return (
     <>
       <Header />
-      <CategoryNav />
       <main className="bg-gray-50 dark:bg-gray-900 min-h-screen">
         {children}
       </main>
